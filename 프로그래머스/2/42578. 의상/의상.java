4c4907270c1a2297e99d13acad8cacc6e665d407
Wongi Kim <cylin0201@naver.com>
2026-03-28 15:58:59 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        int res = 1;
+        for (Map.Entry<String, Integer> entry: map.entrySet()){
+            res *= (entry.getValue() + 1);
+        }
+        return res - 1;
+    }
+}
