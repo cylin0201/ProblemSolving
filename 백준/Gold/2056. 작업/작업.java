9aa0e3de987d3dfe90a470cc4d255f5c50409baf
Wongi Kim <cylin0201@naver.com>
2026-04-03 11:21:44 +0900

    [Gold IV] Title: 작업, Time: 668 ms, Memory: 77944 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static int N;
+    static int[] deg, time, dp;
+    static List<Integer>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        deg = new int[N + 1];
+        time = new int[N + 1];
+        dp = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++)
+            graph[i] = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            time[i] = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            for (int j = 0 ; j < n; j++){
+                int node = Integer.parseInt(st.nextToken());
+                graph[node].add(i);
+                deg[i]++;
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++){
+            if (deg[i] == 0) q.offer(i);
+            dp[i] = time[i];
+        }
+
+        while (!q.isEmpty()){
+            int cur = q.poll();
+
+            for (int next: graph[cur]){
+                dp[next] = Math.max(dp[next], dp[cur] + time[next]);
+                deg[next] --;
+                if (deg[next] == 0){
+                    q.offer(next);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <=N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
