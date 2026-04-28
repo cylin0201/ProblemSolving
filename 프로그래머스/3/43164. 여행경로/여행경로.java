3fc4bd18d95580c9773679c0d481817cc8622438
Wongi Kim <cylin0201@naver.com>
2026-04-28 21:03:05 +0900

    [level 3] Title: 여행경로, Time: 145.75 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    boolean[] visited;
+    List<String> allRoutes;
+
+    public String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];
+        allRoutes = new ArrayList<>();
+        
+        dfs("ICN", "ICN", tickets, 0);        
+        Collections.sort(allRoutes);
+        
+        return allRoutes.get(0).split(" ");
+    }
+    
+    void dfs(String node, String route, String[][] tickets, int count) {
+        if (count == tickets.length) {
+            allRoutes.add(route);
+            return;
+        }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i][0].equals(node) && !visited[i]) {
+                visited[i] = true;
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
